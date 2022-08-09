80699da7def25738cd2d1a2936edca7ef398b3ca
Fred Jiang <jiangf@shinetechsoftware.com>
2022-08-09 14:44:41 +0800

    advanced type

--- TsPlay/src/index.ts
+++ TsPlay/src/index.ts
@@ -1,27 +1,44 @@
-let num: number = 123_456_789;
-console.log(num);
-
-let numbers: number[] = [];
-let user: [number, string] = [1, 'Fred'];
-
-enum Size {
-  Small = 1,
-  Medium,
-  Large,
-}
-let mySize: Size = Size.Small;
-console.log(mySize);
-
-function cal(icome: number, tax: number = 2021) {
-  let rate = 1.3;
-  if (tax > 2022) return icome * rate;
-
-  return icome * (rate + 0.1);
-}
-
-let employee: {
+// Type aliases
+type Employee = {
   readonly id: number;
   name: string;
   nickname?: string;
-} = { id: 1, name: 'Fred' };
-employee.nickname = 'LaoGanMa';
+  retire: (date: Date) => void;
+};
+
+let emp: Employee = {
+  id: 1,
+  name: 'Fred',
+  retire: (date: Date) => console.log(date),
+};
+
+// Union type
+function kgToLbs(weight: number | string): number {
+  if (typeof weight === 'number') return weight * 2.2;
+
+  return parseInt(weight) * 2.2;
+}
+
+// Intersection Type
+let weight: number & string;
+type Draggable = {
+  drag: () => void;
+};
+
+type Resizable = {
+  resize: () => void;
+};
+
+type UIWidget = Draggable & Resizable;
+let textBox: UIWidget = {
+  drag: () => {},
+  resize: () => {},
+};
+
+// Type narrowing
+
+// Nullable types
+
+// The unknown type
+
+// The never type
